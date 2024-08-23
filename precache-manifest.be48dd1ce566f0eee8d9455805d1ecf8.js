b7b5ade078be27a5a4bfd55558176c643d63f327
aidanroley <aidanroley@icloud.com>
2024-08-23 01:14:20 -0400

    Updates

---
+++ precache-manifest.be48dd1ce566f0eee8d9455805d1ecf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ff44c1374812747573a9b1e26ae373",
+    "url": "/collections-app/index.html"
+  },
+  {
+    "revision": "1362e34846cc48afa795",
+    "url": "/collections-app/static/css/main.94082efc.chunk.css"
+  },
+  {
+    "revision": "678e2006add6df85c3bd",
+    "url": "/collections-app/static/js/2.1073a26c.chunk.js"
+  },
+  {
+    "revision": "f9c194d2e40ce5f2b52be28f0ca181f4",
+    "url": "/collections-app/static/js/2.1073a26c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e66d7ecfc762fab5294",
+    "url": "/collections-app/static/js/3.7b03150e.chunk.js"
+  },
+  {
+    "revision": "1362e34846cc48afa795",
+    "url": "/collections-app/static/js/main.82e580ec.chunk.js"
+  },
+  {
+    "revision": "ea079a7566228bd0695b",
+    "url": "/collections-app/static/js/runtime-main.f165b60d.js"
+  }
+]);
